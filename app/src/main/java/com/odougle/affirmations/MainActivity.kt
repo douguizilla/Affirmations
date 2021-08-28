df7ff76622ded6ac8c49528d8e03f7787d7fee7d
douguizilla <douglasgommez@gmail.com>
2021-08-28 17:16:37 -0300

    put recycler view on MainActivity.kt

--- app/src/main/java/com/odougle/affirmations/MainActivity.kt
+++ app/src/main/java/com/odougle/affirmations/MainActivity.kt
@@ -2,10 +2,20 @@ package com.odougle.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.odougle.affirmations.adapter.ItemAdapter
+import com.odougle.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
     }
 }
